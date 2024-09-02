65cfb3c3a59ff2b9647f795ca1c2d01b298cb946
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-02 11:29:16 +0200

    add function drawbricks

--- breakout.js
+++ breakout.js
@@ -71,3 +71,21 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095dd";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
