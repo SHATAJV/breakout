66e149be5fd0b2b6b448aeb7fe3fd72665b1ad19
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-02 11:14:22 +0200

    add setting de rectangle

--- breakout.js
+++ breakout.js
@@ -9,5 +9,9 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+// setting de rectangle 
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
 
 
